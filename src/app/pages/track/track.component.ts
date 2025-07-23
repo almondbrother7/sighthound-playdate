6eef128a6dd825480924c2b87751ed5f403dd98a
Andrew Carey <almondbrother7@gmail.com>
2025-07-23 19:51:01 -0400

    Update scrolling and some pics

--- src/app/pages/track/track.component.ts
+++ src/app/pages/track/track.component.ts
@@ -6,65 +6,102 @@ import { Component } from '@angular/core';
   styleUrls: ['./track.component.css']
 })
 export class TrackComponent {
-  galleryImages = [
-      { src: 'assets/images/track/turn1.jpg', caption: 'Track view 1 '},
-      { src: 'assets/images/track/turn2.jpg', caption: 'Track view 2 '},
-      { src: 'assets/images/track/turn3.jpg', caption: 'Track view 3 '},
-      { src: 'assets/images/track/turn4.jpg', caption: 'Track view 4 '},
-      { src: 'assets/images/track/turn5.jpg', caption: 'Track view 5 '},
-      { src: 'assets/images/track/turn6.jpg', caption: 'Track view 6 '},
-      { src: 'assets/images/track/starting-gate.jpg', caption: 'Track view 7 '},
-      { src: 'assets/images/track/front-straight.jpg', caption: 'Track view 8 '},
-      { src: 'assets/images/track/bamboo-shortcut.jpg', caption: 'Track view 9 '},
-      { src: 'assets/images/track/bamboo-straight.jpg', caption: 'Track view 10 '},
-      { src: 'assets/images/track/bamboo-straight2.jpg', caption: 'Track view 11 '},
-      { src: 'assets/images/track/banana-shortcut.jpg', caption: 'Track view 12 '},
-      { src: 'assets/images/track/deck-straight.jpg', caption: 'Track view 13 '},
-      { src: 'assets/images/track/east-straight.jpg', caption: 'Track view 14 '},
-      { src: 'assets/images/track/front-straight.jpg', caption: 'Track view 15 '},
-      { src: 'assets/images/track/hound-playground.jpg', caption: 'Track view 16 '},
-      { src: 'assets/images/track/playground.jpg', caption: 'Track view 17 '},
-      { src: 'assets/images/track/playground2.jpg', caption: 'Track view 18 '},
-      { src: 'assets/images/track/track1-backside.jpg', caption: 'Track view 19 '},
-      { src: 'assets/images/track/viewing-gallery.jpg', caption: 'Track view 20 '},
-      { src: 'assets/images/track/west-return.jpg', caption: 'Track view 21 '},
-      { src: 'assets/images/track/north-straight.jpg', caption: 'Track view 22 '},
-      { src: 'assets/images/track/west-straight.jpg', caption: 'Track view 23 '},
 
-      { src: 'assets/images/grounds/grounds1.jpg', caption: 'Track view 24 '},
-      { src: 'assets/images/grounds/grounds2.jpg', caption: 'Track view 25 '},
-      { src: 'assets/images/grounds/grounds3.jpg', caption: 'Track view 26 '},
-      { src: 'assets/images/grounds/grounds4.jpg', caption: 'Track view 27 '},
-      { src: 'assets/images/grounds/grounds5.jpg', caption: 'Track view 28 '},
-      { src: 'assets/images/grounds/grounds6.jpg', caption: 'Track view 29 '},
-      { src: 'assets/images/grounds/grounds7.jpg', caption: 'Track view 30 '},
-      { src: 'assets/images/grounds/grounds8.jpg', caption: 'Track view 31 '},
-      { src: 'assets/images/grounds/grounds9.jpg', caption: 'Track view 32 '},
-      { src: 'assets/images/grounds/grounds10.jpg', caption: 'Track view 33 '},
-      { src: 'assets/images/grounds/grounds11.jpg', caption: 'Track view 34 '},
-      { src: 'assets/images/grounds/grounds12.jpg', caption: 'Track view 35 '},
-      { src: 'assets/images/grounds/grounds13.jpg', caption: 'Track view 36 '},
-      { src: 'assets/images/grounds/grounds14.jpg', caption: 'Track view 37 '},
-      { src: 'assets/images/grounds/grounds15.jpg', caption: 'Track view 38 '},
-      { src: 'assets/images/grounds/grounds16.jpg', caption: 'Track view 39 '},
-      { src: 'assets/images/grounds/grounds17.jpg', caption: 'Track view 40 '},
-      { src: 'assets/images/grounds/grounds18.jpg', caption: 'Track view 41 '},
-      { src: 'assets/images/grounds/grounds19.jpg', caption: 'Track view 42 '},
-      { src: 'assets/images/grounds/grounds20.jpg', caption: 'Track view 43 '},
-      { src: 'assets/images/grounds/grounds21.jpg', caption: 'Track view 44 '},
-      { src: 'assets/images/grounds/grounds22.jpg', caption: 'Track view 45 '},
+  constructor() {}
+
+  galleryImages = [
+      { src: 'assets/images/track/turn1.jpg', caption: 'Turn 1 '},
+      { src: 'assets/images/track/turn2.jpg', caption: 'Turn 2 '},
+      { src: 'assets/images/track/turn3.jpg', caption: 'Turn 3 '},
+      { src: 'assets/images/track/turn4.jpg', caption: 'Turn 4 '},
+      { src: 'assets/images/track/turn5.jpg', caption: 'Turn 5 '},
+      { src: 'assets/images/track/turn6.jpg', caption: 'Turn 6 '},
+      { src: 'assets/images/track/starting-gate.jpg', caption: 'Starting Gate'},
+      { src: 'assets/images/track/front-straight.jpg', caption: 'Front Straight'},
+      { src: 'assets/images/track/bamboo-shortcut.jpg', caption: 'Bamboo Shortcut'},
+      { src: 'assets/images/track/bamboo-straight.jpg', caption: 'Bamboo Straight '},
+      { src: 'assets/images/track/banana-shortcut.jpg', caption: 'Banana Shortcut '},
+      { src: 'assets/images/track/deck-cut-through.jpg', caption: 'Deck Straight'},
+      { src: 'assets/images/track/east-straight.jpg', caption: 'East Straight'},
+      { src: 'assets/images/track/south-straight.jpg', caption: 'South Straight'},
+      { src: 'assets/images/track/hound-playground.jpg', caption: 'Hound Playground'},
+      { src: 'assets/images/track/playground.jpg', caption: 'Hound Playground'},
+      { src: 'assets/images/track/playground2.jpg', caption: 'Hound Playground'},
+      { src: 'assets/images/track/track1-backside.jpg', caption: 'Track 1 Backside'},
+      { src: 'assets/images/track/viewing-gallery.jpg', caption: 'Viewing Gallery'},
+      { src: 'assets/images/track/west-return.jpg', caption: 'West Return'},
+      { src: 'assets/images/track/north-straight.jpg', caption: 'North Straight'},
+      { src: 'assets/images/track/west-straight.jpg', caption: 'West Straight'},
+      { src: 'assets/images/track/come-and-play.jpg', caption: 'Hound Playground'},
+      { src: 'assets/images/track/found-ya.jpg', caption: 'Found Ya'},
+      { src: 'assets/images/track/north-straight.jpg', caption: 'North Straight'},
+      { src: 'assets/images/track/pack-area.jpg', caption: 'Pack Area'},
+      { src: 'assets/images/track/playground-3.jpg', caption: 'Hound Playground'},
+      { src: 'assets/images/track/playsafe-groud-cover.jpg', caption: 'Playsafe Mulch'},
+      { src: 'assets/images/track/track1-return.jpg', caption: 'Track 1 Return'},
+      { src: 'assets/images/track/track1-walk-through.jpg', caption: 'Track 1 Walkthru'},
+      { src: 'assets/images/track/track2-walk-through.jpg', caption: 'Track 2 Walkthru'},
+      { src: 'assets/images/track/track2-approach.jpg', caption: 'Track 2 Approach'},
+      { src: 'assets/images/track/track2.jpg', caption: 'Track 2'},
+      { src: 'assets/images/track/turn4-out.jpg', caption: 'Turn 4 Out'},
+      { src: 'assets/images/track/turn5-approach.jpg', caption: 'Turn 5 Approach'},
+      { src: 'assets/images/track/viewing-area.jpg', caption: 'Viewing Area'},
+      { src: 'assets/images/track/viewing-gallery.jpg', caption: 'Viewing Gallery'},
+
+      { src: 'assets/images/grounds/african-blue-basil.jpg', caption: 'African Blue Basil'},
+      { src: 'assets/images/grounds/asian-hibiscus.jpg', caption: 'Asian Hibiscus'},
+      { src: 'assets/images/grounds/bamboo.jpg', caption: 'Bamboo'},
+      { src: 'assets/images/grounds/bananas.jpg', caption: 'Banana'},
+      { src: 'assets/images/grounds/barbados-cherry.jpg', caption: 'Barbados Cherry'},
+      { src: 'assets/images/grounds/blanket-flower.jpg', caption: 'Blanket Flower'},
+      { src: 'assets/images/grounds/butterfly-pea.jpg', caption: 'Butterfly Pea'},
+      { src: 'assets/images/grounds/chili-peppers.jpg', caption: 'Chili Peppers'},
+      { src: 'assets/images/grounds/coral-vine.jpg', caption: 'Coral- Vine'},
+      { src: 'assets/images/grounds/cranberry-hibuscus.jpg', caption: 'Cranberry Hibuscus'},
+      { src: 'assets/images/grounds/curcoma.jpg', caption: 'Curcoma'},
+      { src: 'assets/images/grounds/fig.jpg', caption: 'Fig'},
+      { src: 'assets/images/grounds/firebush.jpg', caption: 'Girebush'},
+      { src: 'assets/images/grounds/geraniums.jpg', caption: 'Geraniums'},
+      { src: 'assets/images/grounds/golden-trumpet.jpg', caption: 'Golden Trumpet'},
+      { src: 'assets/images/grounds/honeykiss-mango.jpg', caption: 'Honeykiss Mango'},
+      { src: 'assets/images/grounds/japanese-maple.jpg', caption: 'Japanese Maple'},
+      { src: 'assets/images/grounds/jatropha.jpg', caption: 'Jatropha'},
+      { src: 'assets/images/grounds/kent-mango.jpg', caption: 'Kent Mango'},
+      { src: 'assets/images/grounds/mahachinok-mango.jpg', caption: 'Mahachinok Mango'},
+      { src: 'assets/images/grounds/marigolds.jpg', caption: 'Marigolds'},
+      { src: 'assets/images/grounds/mexican-sunflower.jpg', caption: 'Mexican Sunflower'},
+      { src: 'assets/images/grounds/milk-weed.jpg', caption: 'Milkweed'},
+      { src: 'assets/images/grounds/papayas.jpg', caption: 'Papayas'},
+      { src: 'assets/images/grounds/paper-lily.jpg', caption: 'Paper Lily'},
+      { src: 'assets/images/grounds/pineapples.jpg', caption: 'Pineapples'},
+      { src: 'assets/images/grounds/porter-weed.jpg', caption: 'Porterweed'},
+      { src: 'assets/images/grounds/shade-structure.jpg', caption: 'Shade Structure'},
+      { src: 'assets/images/grounds/sidewalk-cut-through.jpg', caption: 'Sidewalk Cut Through'},
+      { src: 'assets/images/grounds/song-of-india.jpg', caption: 'Song Of India'},
+      { src: 'assets/images/grounds/spider-lily.jpg', caption: 'Spider Lily'},
+      { src: 'assets/images/grounds/star-jasmine.jpg', caption: 'Star Jasmine'},
+      { src: 'assets/images/grounds/wild-lime.jpg', caption: 'Wild Lime'},
+      { src: 'assets/images/grounds/wild-passion-vine.jpg', caption: 'Wild Passion Vine'},
+      { src: 'assets/images/grounds/your-pack.jpg', caption: 'your-pack'},
 
     ];
 
   visibleLightbox = false;
   clickedIndex = 0;
-  visibleCount = 6;
-  increment = 6;
+  visibleCount = 8;
+  increment = 8;
 
   loadMore() {
     this.visibleCount += this.increment;
+
+    // Wait a bit for Angular to render the new DOM
     setTimeout(() => {
-      window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+      const scrollHeight = document.documentElement.scrollHeight;
+
+      window.scrollTo({
+        top: scrollHeight,
+        behavior: 'smooth'
+      });
     }, 50);
   }
 
