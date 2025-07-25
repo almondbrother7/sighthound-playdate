a5651d5c1f75b72ba23bc8a072a261748116e67c
Andrew Carey <almondbrother7@gmail.com>
2025-07-25 16:30:15 -0400

    Added mini-lightbox

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Output } from '@angular/core';
 import { trigger, style, animate, transition } from '@angular/animations';
+import { outputAst } from '@angular/compiler';
 
 @Component({
   selector: 'app-home',
@@ -14,4 +15,25 @@ import { trigger, style, animate, transition } from '@angular/animations';
     ])
   ]
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  julioGallery = [
+    { src: 'assets/images/julio1.jpg', caption: 'Julio stride 1' },
+    { src: 'assets/images/julio2.jpg', caption: 'Julio stride 2' },
+    { src: 'assets/images/julio3.jpg', caption: 'Julio stride 3' },
+    { src: 'assets/images/julio4.jpg', caption: 'Julio stride 4' }
+  ];
+
+  visibleLightbox = false;
+  clickedIndex = 0;
+  visibleCount = 4;
+
+  openLightbox(index: number) {
+    this.clickedIndex = index;
+    this.visibleLightbox = true;
+  }
+
+
+
+
+}
